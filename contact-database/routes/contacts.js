ff520fac2f3268ea5a9d0f265928a3901bdfe4fb
vinay.katta <vinay.katta@servicenow.com>
2023-02-05 20:36:19 +0530

    Lab2: View and Post data to contacts list page

--- contact-database/routes/contacts.js
+++ contact-database/routes/contacts.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 const contactsRepo = require('../src/contactsRepository');
 
-let data = [{
+let data = [
+  {text : 'This is a todo 1 test' , id:'e35a1473-de27-44cc-9de7-a6304e78f976'},
+   {text : 'This is a todo 2 test' , id:'7eabce2a-bf5e-4586-bcad-d8102efa8f77'}
 
-}];
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contacts', { heading: 'List Of All Contacts' });
+  const data = contactsRepo.findAll();
+  console.log(data);
+  res.render('contacts', { heading: 'List Of All Contacts', contacts:data });
 });
 
 /* GET Contact page. */
@@ -28,7 +32,7 @@ router.post('/add', function(req, res, next) {
     let email = req.body.emailAddress.trim();
     let notes = req.body.notes.trim();
     console.log(firstName + ' ' +lastName +' ' + email + ' ' + notes); 
-    contactsRepo.create({text: req.body.firstName.trim()});
+    contactsRepo.create({text: req.body.firstName.trim()})
     res.redirect("/contacts");
   }
   
